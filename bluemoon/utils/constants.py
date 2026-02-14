b414d6ea5abaff4d6ed03d86373681a783fff9ef
Aarav Maloo <aaravmaloo06@gmail.com>
2026-02-14 11:52:46 +0530

    create constants with util games

---
+++ bluemoon/utils/constants.py
@@ -0,0 +1,30 @@
+PROFANITY_WORDS = {
+    "slur1",
+    "slur2",
+    "abuse1",
+    "abuse2",
+}
+
+TRUTHS = [
+    "What is one secret goal you never told anyone?",
+    "What message did you regret sending?",
+    "What habit are you trying to quit?",
+]
+
+DARES = [
+    "Send a funny emoji-only message in general chat.",
+    "Compliment the next person who types.",
+    "Change your nickname for 10 minutes.",
+]
+
+WOULD_YOU_RATHER = [
+    ("Have unlimited music", "Have unlimited games"),
+    ("Be famous online", "Be rich offline"),
+    ("Lose your phone", "Lose your wallet"),
+]
+
+TRIVIA = [
+    ("What planet is known as the Red Planet?", "mars"),
+    ("What language does Discord.py use?", "python"),
+    ("What is 9 * 9?", "81"),
+]
